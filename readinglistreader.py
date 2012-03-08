19869f434e7f97a9cd0f221d38c9a35123d22ceb
Jim DeVona <jim.devona@gmail.com>
2012-03-07 22:28:12 -0500

    Revised readinglistreader.py to use readinglistlib (use previous commit for fully self-contained tool)

--- readinglistreader.py
+++ readinglistreader.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
 import os
-import sys
-import shlex
 import argparse
 import datetime
-import subprocess
-import plistlib
+
+from readinglistlib import ReadingListReader
 
 # Configure CLI
 fields = ['title', 'url', 'preview', 'date', 'added', 'viewed', 'uuid', 'synckey', 'syncserverid']
@@ -32,54 +30,13 @@ if 'tab' == args.separator:
 # Input
 if not os.path.exists(args.input):
 	raise SystemExit, "The input file does not exist: %s" % args.input
-cmd = shlex.split('/usr/bin/plutil -convert xml1 -o - %s' % args.input)
-input_pipe = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE).stdout
-try:
-	xml_plist = plistlib.readPlist(input_pipe)
-except:
-	raise SystemExit, "An error occured while attempting to parse the input. (%s)" % sys.exc_info()[1]
-input_pipe.close()
+rlr = ReadingListReader(args.input)
 
-# Locate reading list
-rl_section = filter(lambda record: 'com.apple.ReadingList' == record.get('Title'), xml_plist['Children'])
-reading_list = rl_section[0].get('Children')
-if reading_list == None:
-	reading_list = []
-
-# assemble our own list of reading list items
-bookmarks = []
-for item in reading_list:
-	
-	# date added (via iCloud) and date viewed (read) may not be defined.
-	# we use a long-ago time as undef placeholder to facilitate sorting, etc.
-	added = item['ReadingList'].get('DateAdded')
-	if None == added:
-		added = datetime.datetime.min
-	viewed = item['ReadingList'].get('DateLastViewed')
-	if None == viewed:
-		viewed = datetime.datetime.min
-
-	bookmarks.append({
-			'title': item['URIDictionary']['title'],
-			'url': item['URLString'],
-			'preview': item['ReadingList']['PreviewText'],
-			'date': item['ReadingList']['DateLastFetched'],
-			'added': added,
-			'viewed': viewed,
-			'uuid': item['WebBookmarkUUID'],
-			'synckey': item['Sync'].get('Key'),
-			'syncserverid': item['Sync'].get('ServerID')})
-
-# Apply filtration (test LastViewedDate to determine unread status).
-if 'unread' == args.show:
-	bookmarks = filter(lambda record: datetime.datetime.min == record['viewed'], bookmarks)
-elif 'read' == args.show:
-	bookmarks = filter(lambda record: datetime.datetime.min != record['viewed'], bookmarks)
-
-# Apply sortation
-bookmarks = sorted(bookmarks, key=lambda record: record[args.sortfield])
-if 'descending' == args.sortorder:
-	bookmarks.reverse()
+bookmarks = rlr.read(
+		show = None if 'all' == args.show else args.show,
+		sortfield = args.sortfield,
+		ascending = True if 'ascending' == args.sortorder else False,
+		dateformat = args.timestamp)
 
 if args.bookmarks:
 
@@ -118,16 +75,8 @@ else:
 		field_values = []
 		
 		for field in args.fields:
-			field_value = bookmark[field]
-			
-			# Convert timestamps, if defined, to printable strings.
-			if 'date' == field or 'added' == field or 'viewed' == field:
-				if datetime.datetime.min == field_value:
-					field_value = ''
-				else:
-					field_value = field_value.strftime(args.timestamp)
-			
+			field_value = bookmark[field]			
 			field_values.append(field_value.encode('utf-8'))
-					
+		
 		output_record(field_values)
 	
