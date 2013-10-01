c0a50583ac7e1180e5a76f903fe21991f67aead5
Jim DeVona <jim.devona@gmail.com>
2013-10-01 09:53:34 -0400

    draft distutils setup script for readinglistlib

---
+++ readinglistlib/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name = 'readinglistlib',
+	version = '0.1',
+	description = 'Python module to read contents of Safari Reading List.',
+	author = 'Jim DeVona',
+	author_email = 'jim@anoved.net',
+	url = 'https://github.com/anoved/ReadingListReader',
+	license = 'MIT License',
+	platforms = ['darwin'],
+	py_modules = ['readinglistlib']
+);
